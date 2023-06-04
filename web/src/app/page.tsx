49366f4cf1e30454be5844af67175c9c0fd41dff
Luyn4 <luyna1@hotmail.com>
2023-06-04 15:35:40 -0300

    add mudancas

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <main>
-      <h1 className="text-red-500">Hello World</h1>
+      <h1 className="text-red-500">Hello World Mudando Para Testar</h1>
     </main>
   )
 }
